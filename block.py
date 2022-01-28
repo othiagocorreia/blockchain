a111abc5ba472654347fc8eb833304b27f5f292c
oThiagoCorreia <68630379+othiagocorreia@users.noreply.github.com>
2022-01-28 14:06:24 -0300

    Update block.py

--- block.py
+++ block.py
@@ -10,12 +10,19 @@ class Block():
         self.hash = self.generateHash()
 
     def generateHash(self):
-        blockData = str(self.transactions) + str(self.timestamps) + str(self.prevHash)
+        #nonce = 1
+        #while not self.block.isValid(blockHash):
+        blockData = str(self.transactions) + str(self.timestamps) + str(self.prevHash) #+str(self.nonce)
         blockHash = hashlib.sha256(blockData.encode()).hexdigest()
+        #nonce incrementado
         return blockHash
+    
+    #def isValid(self, hash):
+        #return self.hash.startswith('0000')
 
     def showBlock(self):
         print("Transações: ", self.transactions)
         print("Hora e Data: ", self.timestamps)
         print("Hash do bloco: ", self.hash)
         print("Hash Anterior: ", self.prevHash)
+        #print("Nonce: ", self.nonce)
