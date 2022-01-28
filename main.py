8864ef528ebe2761ca6d8a5212478c31a3f326dc
oThiagoCorreia <68630379+othiagocorreia@users.noreply.github.com>
2022-01-28 14:07:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from chain import Blockchain
 
+#t = int(input("Quantas transações você quer fazer?"))
 sender = str(input("Qual o seu nome?"))
 amount = str(input("Quantos Bitcoins você quer transferir?"))
 recipient = str(input("Para quem você quer transferir?"))
 
 transaction = sender + " transferiu: " + amount + " Bitcoin(s) para " + recipient 
-
 
 Blockchain.addBlock(transaction)
 Blockchain.showBlocks()
